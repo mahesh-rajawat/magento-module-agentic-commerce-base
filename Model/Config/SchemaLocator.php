5a2f3bb3df72110bc6519d190941c66a8adecd80
Mahesh Singh <mahesh.singh@pinja.com>
2026-04-22 10:40:40 +0300

    Fixing config reated issue

--- Model/Config/SchemaLocator.php
+++ Model/Config/SchemaLocator.php
@@ -5,12 +5,38 @@ declare(strict_types=1);
 namespace MSR\AgenticUcp\Model\Config;
 
 use Magento\Framework\Config\SchemaLocatorInterface;
+use Magento\Framework\Module\Dir;
+use Magento\Framework\Module\Dir\Reader;
 
 /**
  * Locates the XSD schema for ucp.xml validation.
  */
 class SchemaLocator implements SchemaLocatorInterface
 {
+    public const CONFIG_FILE_SCHEMA = 'ucp.xsd';
+
+    /**
+     * Path to corresponding XSD file with validation rules for merged config
+     *
+     * @var string
+     */
+    protected $schema = null;
+    /**
+     * Path to corresponding XSD file with validation rules for separate config files
+     * @var string
+     */
+    protected $perFileSchema = null;
+
+    /**
+     * @param Reader $moduleReader
+     */
+    public function __construct(Reader $moduleReader)
+    {
+        $configDir = $moduleReader->getModuleDir(Dir::MODULE_ETC_DIR, 'MSR_AgenticUcp');
+        $this->schema = $configDir . DIRECTORY_SEPARATOR . self::CONFIG_FILE_SCHEMA;
+        $this->perFileSchema = $configDir . DIRECTORY_SEPARATOR . self::CONFIG_FILE_SCHEMA;
+    }
+
     /**
      * Get the path to the full XSD schema.
      *
@@ -18,7 +44,7 @@ class SchemaLocator implements SchemaLocatorInterface
      */
     public function getSchema(): ?string
     {
-        return BP . '/app/code/MSR/AgenticUcp/etc/ucp.xsd';
+        return $this->schema;
     }
 
     /**
